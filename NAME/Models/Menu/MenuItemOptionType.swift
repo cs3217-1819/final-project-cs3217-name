258d8eb1b50a2b861902e297334599a59da1ab22
E-Liang Tan <eliang@eliangtan.com>
2019-03-27 18:07:52 +0800

    Make MenuItemOptionType conform to Codable

--- NAME/Models/Menu/MenuItemOptionType.swift
+++ NAME/Models/Menu/MenuItemOptionType.swift
@@ -1,5 +1,38 @@
-enum MenuItemOptionType {
+import Foundation
+
+enum MenuItemOptionType: Codable {
     case boolean(Bool)
     case quantity(Int)
     case multipleChoice([String])
+
+    enum CodingKeys: CodingKey {
+        case boolean
+        case quantity
+        case multipleChoice
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        if let boolean = try? container.decode(Bool.self, forKey: .boolean) {
+            self = .boolean(boolean)
+        } else if let boolean = try? container.decode(Int.self, forKey: .quantity) {
+            self = .quantity(boolean)
+        } else if let multipleChoice = try? container.decode([String].self, forKey: .multipleChoice) {
+            self = .multipleChoice(multipleChoice)
+        }
+        // TODO: Replace this NSError with a customized one
+        throw NSError()
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .boolean(let boolean):
+            try container.encode(boolean, forKey: .boolean)
+        case .quantity(let quantity):
+            try container.encode(quantity, forKey: .quantity)
+        case .multipleChoice(let choices):
+            try container.encode(choices, forKey: .multipleChoice)
+        }
+    }
 }
