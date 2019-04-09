25d92bc73c849d6331a7729dc4fe26f113a78fac
Caryn Heng <heng.zhixuan.caryn@dhs.sg>
2019-04-09 09:40:36 +0800

    Add computed properties that is useful for kitchen backlog (#35)

--- NAME/Models/Order/Order.swift
+++ NAME/Models/Order/Order.swift
@@ -7,6 +7,36 @@ class Order: Object {
     @objc dynamic var id: String = UUID().uuidString
     @objc dynamic var queueNumber: Int = 0
 
+    var orderReceivedTimeStamp: Date? {
+        return orderItems
+            .flatMap { $0.orderHistories.filter { $0.state == .received } }
+            .map { $0.timestamp }
+            .min()
+    }
+
+    var isOrderReady: Bool {
+        return readyStateOrderItems.count + completedStateOrderItems.count == orderItems.count
+    }
+
+    var isOrderCompleted: Bool {
+        return completedStateOrderItems.count == orderItems.count
+    }
+
+    /// All Order Items that are currently at recieved state of lifecycle.
+    var receivedStateOrderItems: [OrderItem] {
+        return orderItems.filter { $0.orderHistories.last?.state == .received }
+    }
+
+    /// All Order Items that are currently at ready state of lifecycle.
+    var readyStateOrderItems: [OrderItem] {
+        return orderItems.filter { $0.orderHistories.last?.state == .ready }
+    }
+
+    /// All Order Items that are currently at completed state of lifecycle.
+    var completedStateOrderItems: [OrderItem] {
+        return orderItems.filter { $0.orderHistories.last?.state == .completed }
+    }
+
     // MARK: - Relationship
     @objc dynamic var customer: Customer?
     let orderItems = LinkingObjects(fromType: OrderItem.self, property: "order")
